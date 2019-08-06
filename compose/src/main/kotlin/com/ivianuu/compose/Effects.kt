8eb81e5a794b9f7e6fee5c4a7d6dd7e257ee3587
IVIanuu <ivianuu@gmail.com>
2019-08-07 01:49:31 +0200

    Better state

--- compose/src/main/kotlin/com/ivianuu/compose/Effects.kt
+++ compose/src/main/kotlin/com/ivianuu/compose/Effects.kt
@@ -3,7 +3,9 @@ package com.ivianuu.compose
 import androidx.compose.Ambient
 import androidx.compose.CommitScope
 import androidx.compose.Effect
+import androidx.compose.invalidate
 import androidx.compose.unaryPlus
+import kotlin.reflect.KProperty
 
 fun <T, V1> ViewComposition.key(v1: V1, block: Effect<T>.() -> T) =
     +androidx.compose.key(v1 = v1, block = block)
@@ -55,16 +57,60 @@ fun <V1, V2> ViewComposition.onPreCommit(v1: V1, v2: V2, callback: CommitScope.(
 fun ViewComposition.onPreCommit(vararg inputs: Any?, callback: CommitScope.() -> Unit) =
     +androidx.compose.onPreCommit(inputs = *inputs, callback = callback)
 
-fun <T> ViewComposition.state(init: () -> T) = +androidx.compose.state(init = init)
+class State<T> @PublishedApi internal constructor(value: T, val invalidate: () -> Unit) {
+    /* NOTE(lmr): When this module is compiled with IR, we will need to remove the below Framed implementation */
 
-fun <T, V1> ViewComposition.stateFor(v1: V1, init: () -> T) =
-    +androidx.compose.stateFor(v1 = v1, init = init)
+    @Suppress("UNCHECKED_CAST")
+    var value: T = value
+        set(value) {
+            field = value
+            invalidate()
+        }
 
-fun <T, V1, V2> ViewComposition.stateFor(v1: V1, v2: V2, init: () -> T) =
-    +androidx.compose.stateFor(v1 = v1, v2 = v2, init = init)
+    /**
+     * The componentN() operators allow state objects to be used with the property destructuring syntax
+     *
+     * var (foo, setFoo) = +state { 0 }
+     * setFoo(123) // set
+     * foo == 123 // get
+     */
+    operator fun component1(): T = value
 
-fun <T> ViewComposition.stateFor(vararg inputs: Any?, init: () -> T) =
-    +androidx.compose.stateFor(inputs = *inputs, init = init)
+    operator fun component2(): (T) -> Unit = { value = it }
+
+    /**
+     * The getValue/setValue operators allow State to be used as a local variable with a delegate:
+     *
+     * var foo by +state { 0 }
+     * foo += 123 // uses setValue(...)
+     * foo == 123 // uses getValue(...)
+     */
+    operator fun getValue(thisObj: Any?, property: KProperty<*>): T = value
+
+    operator fun setValue(thisObj: Any?, property: KProperty<*>, next: T) {
+        value = next
+    }
+}
+
+fun <T> ViewComposition.state(init: () -> T): State<T> {
+    val invalidate = +invalidate
+    return memo { State(init(), invalidate) }
+}
+
+fun <T, V1> ViewComposition.stateFor(v1: V1, init: () -> T): State<T> {
+    val invalidate = +invalidate
+    return memo(v1) { State(init(), invalidate) }
+}
+
+fun <T, V1, V2> ViewComposition.stateFor(v1: V1, v2: V2, init: () -> T): State<T> {
+    val invalidate = +invalidate
+    return memo(v1, v2) { State(init(), invalidate) }
+}
+
+fun <T> ViewComposition.stateFor(vararg inputs: Any?, init: () -> T): State<T> {
+    val invalidate = +invalidate
+    return memo(*inputs) { State(init(), invalidate) }
+}
 
 fun <T> ViewComposition.model(init: () -> T) = +androidx.compose.model(init = init)
 
--- sample/src/main/kotlin/com/ivianuu/compose/sample/Animation.kt
+++ sample/src/main/kotlin/com/ivianuu/compose/sample/Animation.kt
@@ -2,7 +2,6 @@ package com.ivianuu.compose.sample
 
 import android.animation.ValueAnimator
 import android.view.View
-import androidx.compose.Recompose
 import com.ivianuu.compose.View
 import com.ivianuu.compose.ViewComposition
 import com.ivianuu.compose.layoutRes
@@ -12,38 +11,35 @@ import com.ivianuu.compose.state
 import kotlinx.android.synthetic.main.animation.view.*
 
 fun ViewComposition.Animation() = Route {
-    Recompose { recompose ->
-        var value by state { 0f }
-        onActive {
-            val animation = ValueAnimator()
-            animation.setFloatValues(0f, 1f)
-            animation.repeatMode = ValueAnimator.REVERSE
-            animation.repeatCount = ValueAnimator.INFINITE
+    var value by state { 0f }
+    onActive {
+        val animation = ValueAnimator()
+        animation.setFloatValues(0f, 1f)
+        animation.repeatMode = ValueAnimator.REVERSE
+        animation.repeatCount = ValueAnimator.INFINITE
 
-            animation.addUpdateListener {
-                value = it.animatedFraction
-                recompose()
-            }
-
-            animation.start()
-
-            println("anim: on active")
-
-            onDispose {
-                println("anim: on dispose")
-                animation.cancel()
-            }
+        animation.addUpdateListener {
+            value = it.animatedFraction
         }
 
-        println("anim: rebuild $value")
+        animation.start()
 
-        View<View> {
-            layoutRes(R.layout.animation)
-            updateView {
-                println("anim: update view $value")
-                animation_view.scaleX = value
-                animation_view.scaleY = value
-            }
+        println("anim: on active")
+
+        onDispose {
+            println("anim: on dispose")
+            animation.cancel()
+        }
+    }
+
+    println("anim: rebuild $value")
+
+    View<View> {
+        layoutRes(R.layout.animation)
+        updateView {
+            println("anim: update view $value")
+            animation_view.scaleX = value
+            animation_view.scaleY = value
         }
     }
 }
