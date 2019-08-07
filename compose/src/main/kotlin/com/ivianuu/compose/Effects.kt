2f426bcef1c8fbab0136dd926012a8a78b317de6
IVIanuu <ivianuu@gmail.com>
2019-08-07 02:05:44 +0200

    Better state

--- compose/src/main/kotlin/com/ivianuu/compose/Effects.kt
+++ compose/src/main/kotlin/com/ivianuu/compose/Effects.kt
@@ -4,61 +4,89 @@ import androidx.compose.Ambient
 import androidx.compose.CommitScope
 import androidx.compose.Effect
 import androidx.compose.invalidate
-import androidx.compose.unaryPlus
 import kotlin.reflect.KProperty
 
-fun <T, V1> ViewComposition.key(v1: V1, block: Effect<T>.() -> T) =
-    +androidx.compose.key(v1 = v1, block = block)
+inline fun <T, V1> ViewComposition.key(v1: V1, noinline block: Effect<T>.() -> T) =
+    androidx.compose.key(v1 = v1, block = block).resolve(composer, sourceLocation())
 
-fun <T, V1, V2> ViewComposition.key(v1: V1, v2: V2, block: Effect<T>.() -> T) =
-    +androidx.compose.key(v1 = v1, v2 = v2, block = block)
+inline fun <T, V1, V2> ViewComposition.key(v1: V1, v2: V2, noinline block: Effect<T>.() -> T) =
+    androidx.compose.key(v1 = v1, v2 = v2, block = block).resolve(composer, sourceLocation())
 
-fun <T> ViewComposition.key(vararg inputs: Any?, block: Effect<T>.() -> T) =
-    +androidx.compose.key(inputs = *inputs, block = block)
+inline fun <T> ViewComposition.key(vararg inputs: Any?, noinline block: Effect<T>.() -> T) =
+    androidx.compose.key(inputs = *inputs, block = block).resolve(composer, sourceLocation())
 
-fun <T> memo(calculation: () -> T) = +androidx.compose.memo(calculation = calculation)
+inline fun <T> ViewComposition.memo(noinline calculation: () -> T) =
+    androidx.compose.memo(calculation = calculation).resolve(composer, sourceLocation())
 
-fun <T, V1> ViewComposition.memo(v1: V1, calculation: () -> T) =
-    +androidx.compose.memo(v1 = v1, calculation = calculation)
+inline fun <T, V1> ViewComposition.memo(v1: V1, noinline calculation: () -> T) =
+    androidx.compose.memo(v1 = v1, calculation = calculation).resolve(composer, sourceLocation())
 
-fun <T, V1, V2> ViewComposition.memo(v1: V1, v2: V2, calculation: () -> T) =
-    +androidx.compose.memo(v1 = v1, v2 = v2, calculation = calculation)
+inline fun <T, V1, V2> ViewComposition.memo(v1: V1, v2: V2, noinline calculation: () -> T) =
+    androidx.compose.memo(v1 = v1, v2 = v2, calculation = calculation).resolve(
+        composer,
+        sourceLocation()
+    )
 
-fun <T> ViewComposition.memo(vararg inputs: Any?, calculation: () -> T) =
-    +androidx.compose.memo(inputs = *inputs, calculation = calculation)
+inline fun <T> ViewComposition.memo(vararg inputs: Any?, noinline calculation: () -> T) =
+    androidx.compose.memo(inputs = *inputs, calculation = calculation).resolve(
+        composer,
+        sourceLocation()
+    )
 
-fun ViewComposition.onActive(callback: CommitScope.() -> Unit) =
-    +androidx.compose.onActive(callback = callback)
+inline fun ViewComposition.onActive(noinline callback: CommitScope.() -> Unit) =
+    androidx.compose.onActive(callback = callback).resolve(composer, sourceLocation())
 
-fun ViewComposition.onDispose(callback: () -> Unit) =
-    +androidx.compose.onDispose(callback = callback)
+inline fun ViewComposition.onDispose(noinline callback: () -> Unit) =
+    androidx.compose.onDispose(callback = callback).resolve(composer, sourceLocation())
 
-fun ViewComposition.onCommit(callback: CommitScope.() -> Unit) =
-    +androidx.compose.onCommit(callback = callback)
+inline fun ViewComposition.onCommit(noinline callback: CommitScope.() -> Unit) =
+    androidx.compose.onCommit(callback = callback).resolve(composer, sourceLocation())
 
-fun <V1> ViewComposition.onCommit(v1: V1, callback: CommitScope.() -> Unit) =
-    +androidx.compose.onCommit(v1 = v1, callback = callback)
+inline fun <V1> ViewComposition.onCommit(v1: V1, noinline callback: CommitScope.() -> Unit) =
+    androidx.compose.onCommit(v1 = v1, callback = callback).resolve(composer, sourceLocation())
 
-fun <V1, V2> ViewComposition.onCommit(v1: V1, v2: V2, callback: CommitScope.() -> Unit) =
-    +androidx.compose.onCommit(v1 = v1, v2 = v2, callback = callback)
+inline fun <V1, V2> ViewComposition.onCommit(
+    v1: V1,
+    v2: V2,
+    noinline callback: CommitScope.() -> Unit
+) =
+    androidx.compose.onCommit(v1 = v1, v2 = v2, callback = callback).resolve(
+        composer,
+        sourceLocation()
+    )
 
-fun ViewComposition.onCommit(vararg inputs: Any?, callback: CommitScope.() -> Unit) =
-    +androidx.compose.onCommit(callback = callback)
+inline fun ViewComposition.onCommit(
+    vararg inputs: Any?,
+    noinline callback: CommitScope.() -> Unit
+) =
+    androidx.compose.onCommit(callback = callback).resolve(composer, sourceLocation())
 
-fun ViewComposition.onPreCommit(callback: CommitScope.() -> Unit) =
-    +androidx.compose.onCommit(callback = callback)
+inline fun ViewComposition.onPreCommit(noinline callback: CommitScope.() -> Unit) =
+    androidx.compose.onCommit(callback = callback).resolve(composer, sourceLocation())
 
-fun <V1> ViewComposition.onPreCommit(v1: V1, callback: CommitScope.() -> Unit) =
-    +androidx.compose.onPreCommit(v1 = v1, callback = callback)
+inline fun <V1> ViewComposition.onPreCommit(v1: V1, noinline callback: CommitScope.() -> Unit) =
+    androidx.compose.onPreCommit(v1 = v1, callback = callback).resolve(composer, sourceLocation())
 
-fun <V1, V2> ViewComposition.onPreCommit(v1: V1, v2: V2, callback: CommitScope.() -> Unit) =
-    +androidx.compose.onPreCommit(v1 = v1, v2 = v2, callback = callback)
+inline fun <V1, V2> ViewComposition.onPreCommit(
+    v1: V1,
+    v2: V2,
+    noinline callback: CommitScope.() -> Unit
+) =
+    androidx.compose.onPreCommit(v1 = v1, v2 = v2, callback = callback).resolve(
+        composer,
+        sourceLocation()
+    )
 
-fun ViewComposition.onPreCommit(vararg inputs: Any?, callback: CommitScope.() -> Unit) =
-    +androidx.compose.onPreCommit(inputs = *inputs, callback = callback)
+inline fun ViewComposition.onPreCommit(
+    vararg inputs: Any?,
+    noinline callback: CommitScope.() -> Unit
+) =
+    androidx.compose.onPreCommit(inputs = *inputs, callback = callback).resolve(
+        composer,
+        sourceLocation()
+    )
 
 class State<T> @PublishedApi internal constructor(value: T, val invalidate: () -> Unit) {
-    /* NOTE(lmr): When this module is compiled with IR, we will need to remove the below Framed implementation */
 
     @Suppress("UNCHECKED_CAST")
     var value: T = value
@@ -67,24 +95,10 @@ class State<T> @PublishedApi internal constructor(value: T, val invalidate: () -
             invalidate()
         }
 
-    /**
-     * The componentN() operators allow state objects to be used with the property destructuring syntax
-     *
-     * var (foo, setFoo) = +state { 0 }
-     * setFoo(123) // set
-     * foo == 123 // get
-     */
     operator fun component1(): T = value
 
     operator fun component2(): (T) -> Unit = { value = it }
 
-    /**
-     * The getValue/setValue operators allow State to be used as a local variable with a delegate:
-     *
-     * var foo by +state { 0 }
-     * foo += 123 // uses setValue(...)
-     * foo == 123 // uses getValue(...)
-     */
     operator fun getValue(thisObj: Any?, property: KProperty<*>): T = value
 
     operator fun setValue(thisObj: Any?, property: KProperty<*>, next: T) {
@@ -92,35 +106,41 @@ class State<T> @PublishedApi internal constructor(value: T, val invalidate: () -
     }
 }
 
-fun <T> ViewComposition.state(init: () -> T): State<T> {
+inline fun <T> ViewComposition.state(noinline init: () -> T): State<T> {
     val invalidate = +invalidate
     return memo { State(init(), invalidate) }
 }
 
-fun <T, V1> ViewComposition.stateFor(v1: V1, init: () -> T): State<T> {
+inline fun <T, V1> ViewComposition.stateFor(v1: V1, noinline init: () -> T): State<T> {
     val invalidate = +invalidate
     return memo(v1) { State(init(), invalidate) }
 }
 
-fun <T, V1, V2> ViewComposition.stateFor(v1: V1, v2: V2, init: () -> T): State<T> {
+inline fun <T, V1, V2> ViewComposition.stateFor(v1: V1, v2: V2, noinline init: () -> T): State<T> {
     val invalidate = +invalidate
     return memo(v1, v2) { State(init(), invalidate) }
 }
 
-fun <T> ViewComposition.stateFor(vararg inputs: Any?, init: () -> T): State<T> {
+inline fun <T> ViewComposition.stateFor(vararg inputs: Any?, noinline init: () -> T): State<T> {
     val invalidate = +invalidate
     return memo(*inputs) { State(init(), invalidate) }
 }
 
-fun <T> ViewComposition.model(init: () -> T) = +androidx.compose.model(init = init)
+inline fun <T> ViewComposition.model(noinline init: () -> T) =
+    androidx.compose.model(init = init).resolve(composer, sourceLocation())
 
-fun <T, V1> ViewComposition.modelFor(v1: V1, init: () -> T) =
-    +androidx.compose.modelFor(v1 = v1, init = init)
+inline fun <T, V1> ViewComposition.modelFor(v1: V1, noinline init: () -> T) =
+    androidx.compose.modelFor(v1 = v1, init = init).resolve(composer, sourceLocation())
 
-fun <T, V1, V2> ViewComposition.modelFor(v1: V1, v2: V2, init: () -> T) =
-    +androidx.compose.modelFor(v1 = v1, v2 = v2, init = init)
+inline fun <T, V1, V2> ViewComposition.modelFor(v1: V1, v2: V2, noinline init: () -> T) =
+    androidx.compose.modelFor(v1 = v1, v2 = v2, init = init).resolve(composer, sourceLocation())
 
-fun <T> ViewComposition.modelFor(vararg inputs: Any?, init: () -> T) =
-    +androidx.compose.modelFor(inputs = *inputs, init = init)
+inline fun <T> ViewComposition.modelFor(vararg inputs: Any?, noinline init: () -> T) =
+    androidx.compose.modelFor(inputs = *inputs, init = init).resolve(composer, sourceLocation())
 
-fun <T> ViewComposition.ambient(key: Ambient<T>) = +androidx.compose.ambient(key = key)
+inline fun <T> ViewComposition.ambient(key: Ambient<T>) =
+    androidx.compose.ambient(key = key).resolve(composer, sourceLocation())
+
+@PublishedApi
+internal inline fun <T> Effect<T>.resolve(composer: ViewComposer, key: Any) =
+    resolve(composer, key.hashCode())
