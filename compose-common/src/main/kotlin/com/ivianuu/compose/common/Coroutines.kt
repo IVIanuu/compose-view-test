dc53940a7ad643bf53d0784104abff91c17318e0
IVIanuu <ivianuu@gmail.com>
2019-08-15 17:20:39 +0200

    Moved some stuff around

--- compose-common/src/main/kotlin/com/ivianuu/compose/common/Coroutines.kt
+++ compose-common/src/main/kotlin/com/ivianuu/compose/common/Coroutines.kt
@@ -17,11 +17,79 @@
 package com.ivianuu.compose.common
 
 import com.ivianuu.compose.ComponentComposition
+import com.ivianuu.compose.memo
+import com.ivianuu.compose.onActive
+import com.ivianuu.compose.onCommit
+import com.ivianuu.compose.onDispose
+import com.ivianuu.compose.onPreCommit
 import com.ivianuu.compose.state
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
+
+val ComponentComposition.coroutineScope: CoroutineScope
+    get() = coroutineScope { Dispatchers.Main }
+
+fun ComponentComposition.coroutineScope(context: () -> CoroutineContext): CoroutineScope {
+    val coroutineScope = memo { CoroutineScope(context = context() + Job()) }
+    onDispose { coroutineScope.coroutineContext[Job]!!.cancel() }
+    return coroutineScope
+}
+
+// todo launchOnBindView ?
+
+fun ComponentComposition.launchOnActive(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+) {
+    val coroutineScope = coroutineScope
+    onActive { coroutineScope.launch(context, start, block) }
+}
+
+fun ComponentComposition.launchOnPreCommit(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+) {
+    val coroutineScope = coroutineScope
+    onPreCommit { coroutineScope.launch(context, start, block) }
+}
+
+fun ComponentComposition.launchOnPreCommit(
+    vararg inputs: Any?,
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+) {
+    val coroutineScope = coroutineScope
+    onPreCommit(*inputs) { coroutineScope.launch(context, start, block) }
+}
+
+fun ComponentComposition.launchOnCommit(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+) {
+    val coroutineScope = coroutineScope
+    onCommit { coroutineScope.launch(context, start, block) }
+}
+
+fun ComponentComposition.launchOnCommit(
+    vararg inputs: Any?,
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> Unit
+) {
+    val coroutineScope = coroutineScope
+    onCommit(*inputs) { coroutineScope.launch(context, start, block) }
+}
 
 @BuilderInference
 fun <T> ComponentComposition.load(
@@ -40,5 +108,18 @@ fun <T> ComponentComposition.load(
     launchOnActive(context = context, start = start) {
         state.value = block()
     }
+    return state.value
+}
+
+fun <T> ComponentComposition.flow(
+    flow: Flow<T>
+): T? = flow(null, flow)
+
+fun <T> ComponentComposition.flow(
+    placeholder: T,
+    flow: Flow<T>
+): T {
+    val state = state { placeholder }
+    launchOnActive { flow.collect { state.value = it } }
     return state.value
 }
